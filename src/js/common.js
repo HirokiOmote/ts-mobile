184c1e4e745564b41114d1d0d6d031b281efbabf
HirokiOmote <bubo@bubo.jp>
2017-04-05 19:54:32 +0900

    Rename file & bug fix

--- src/js/common.js
+++ src/js/common.js
@@ -12,11 +12,42 @@ $(() => {
 function btnMenu() {
   const btn = '.menu-trigger';
   const menu = '.globalNavigation';
+  const winWidth = $(window).width();
+  let state = false;
+  let timer = false;
+  let scrollpos;
+  let winWidth_resized;
 
   $(btn).on('click', (event) => {
     event.preventDefault();
     $(btn).toggleClass('active');
     $(menu).toggle(400).toggleClass('active');
+
+    if(state == false) {
+      state = true;
+      scrollpos = $(window).scrollTop();
+      $('body').toggleClass('no-scroll').css({'top': -scrollpos});
+    } else {
+      state = false;
+      $('body').removeClass('no-scroll').css({'top': 0});
+      window.scrollTo(0, scrollpos);
+    }
+
+    $(window).resize(() => {
+      if (timer !== false) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        winWidth_resized = $(window).width();
+
+        if ( winWidth != winWidth_resized ) {
+          $(btn).removeClass('active');
+          $('body').removeClass('no-scroll').css({'top': 0});
+          $(menu).hide(400).removeClass('active');
+          state = false;
+        }
+      }, 200);
+    });
   });
 }
 
